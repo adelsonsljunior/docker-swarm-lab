cb9feda2b5ec0ee3243cad95560316d950e8e3ea
adelsonsljunior <junioradelsonst@gmail.com>
2025-06-02 14:05:41 -0300

    fix: ensure cleanup of temporary stack file in portainer.sh

--- scripts/portainer.sh
+++ scripts/portainer.sh
@@ -2,3 +2,5 @@
 
 curl -L https://downloads.portainer.io/ce-lts/portainer-agent-stack.yml -o portainer-agent-stack.yml
 docker stack deploy -c portainer-agent-stack.yml portainer
+
+rm portainer-agent-stack.yml
